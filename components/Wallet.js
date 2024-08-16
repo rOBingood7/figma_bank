e24523afdf8d641179a750badb241026d6ccb383
Konstantin <kk.developer92@gmail.com>
2024-08-16 18:42:01 +0500

    fixed

--- components/Wallet.js
+++ components/Wallet.js
@@ -1,15 +1,21 @@
+import VanillaTilt from "vanilla-tilt";
+
 export function Wallet(item) {
-  const wallet = document.createElement("div");
-  const type = document.createElement("p");
-  const currency = document.createElement("span");
+	const wallet = document.createElement("div");
+	const type = document.createElement("p");
+	const currency = document.createElement("span");
 
-  wallet.classList.add("wallet", "card1");
-  type.innerHTML = item.name;
-  currency.innerHTML = `${item.currency} | ${Number(item.amount).toLocaleString()}`;
+	wallet.classList.add("wallet", "card1");
+	type.innerHTML = item.name;
+	currency.innerHTML = `${item.currency} | ${Number(item.amount).toLocaleString()}`;
 
-  wallet.onclick = () => {
-    location.assign(`/pages/walletInfo/?id=`+ item.id)
-  }
-  wallet.append(type, currency);
-  return wallet
+	VanillaTilt.init(wallet, {
+		reverse: true,
+	});
+
+	wallet.onclick = () => {
+		location.assign(`/pages/walletInfo/?id=` + item.id)
+	}
+	wallet.append(type, currency);
+	return wallet
 }
--- pages/walletInfo/script.js
+++ pages/walletInfo/script.js
@@ -1,43 +1,60 @@
 import VanillaTilt from "vanilla-tilt";
-import { Wallet } from "../../components/Wallet";
-import { getData, getData_Fixer } from "../../lib/http.request";
-import { reload } from "../../lib/utills";
+import {
+	Wallet
+} from "../../components/Wallet";
+import {
+	getData,
+	getData_Fixer
+} from "../../lib/http.request";
+import {
+	reload
+} from "../../lib/utills";
 
 const currencies = document.querySelectorAll(".currencies");
 const conversion_form = document.querySelector("form");
 const wallet_container = document.querySelector(".wallet_container");
-const localed = JSON.parse(localStorage.getItem("user"));
-const res = await getData("/wallets?userId=" + localed.id);
-const res_currencies = await getData_Fixer("/list");
-const res_conversion = await getData_Fixer("/convert");
 const cards = document.querySelectorAll(".card");
-
 let activeCard = 0;
 
+const localed = JSON.parse(localStorage.getItem("user"));
+const id = location.search.split('=').at(-1)
+
+const res_currencies = await getData_Fixer("/list");
+// const res_conversion = await getData_Fixer("/convert");
+const res = await getData("/wallets/" + id);
+
 cards.forEach((card) => {
-  card.onclick = () => {
-    if (card === activeCard) {
-      card.classList.remove("active");
-      activeCard = 0;
-    } else {
-      if (activeCard) {
-        activeCard.classList.remove("active");
-      }
-      card.classList.add("active");
-      activeCard = card;
-    }
-  };
+	card.onclick = () => {
+		if (card === activeCard) {
+			card.classList.remove("active");
+			activeCard = 0;
+		} else {
+			if (activeCard) {
+				activeCard.classList.remove("active");
+			}
+			card.classList.add("active");
+			activeCard = card;
+		}
+	};
 });
 
-currencies.forEach((select) => {
-  for (let key in res_currencies.data.currencies) {
-    let opt = new Option(`${key}`, key);
-    select.append(opt);
-  }
-});
+if(res_currencies?.data?.currencies) {
+	currencies.forEach((select) => {
+		for (let key in res_currencies.data.currencies) {
+			let opt = new Option(`${key}`, key);
+			select.append(opt);
+		}
+	});
+}
 
 conversion_form.onsubmit = async (e) => {
-  e.preventDefault();
+	e.preventDefault();
+
+	const convertData = {}
+
+	new FormData(e.target).forEach((val, key) => convertData[key] = val)
+
+	console.log(convertData);
 };
 
-reload(res.data, Wallet, wallet_container);
+reload([res.data], Wallet, wallet_container);
