83d8f142d5379ae280f7eb804709fc460d8fea91
Daler <dsarifkulo021@gmail.com>
2024-08-21 18:41:38 +0500

    vite configured

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,18 @@ import {
 
 export default defineConfig({
     build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                addTransaction: resolve(__dirname, 'pages/addTransaction/index.html'),
+                addWallet: resolve(__dirname, 'pages/addWallet/index.html'),
+                signin: resolve(__dirname, 'pages/signin/index.html'),
+                signup: resolve(__dirname, 'pages/signup/index.html'),
+                transactions: resolve(__dirname, 'pages/transactions/index.html'),
+                walletInfo: resolve(__dirname, 'pages/walletInfo/index.html'),
+                wallets: resolve(__dirname, 'pages/wallets/index.html'),
+            },
+        },
         target: "ES2022"
     },
 });
